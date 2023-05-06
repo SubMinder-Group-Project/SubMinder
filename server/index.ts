136069477d436a94d9cf34204a47ea96c108631a
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-05-06 23:19:24 +1200

    changing schadual to be variable updated on Railway.app

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,9 @@
 import server from './server'
+import cron from 'node-cron'
+import { getSubsEventsUsers } from './db/user'
+import { updateEmailStatus } from './db/events'
+import sendReminderEmail from './lib'
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
@@ -6,3 +11,38 @@ server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Server listening on port', PORT)
 })
+
+const cronSchedule = process.env.CRON_SCHEDULE
+
+if (cronSchedule) {
+  cron.schedule(cronSchedule, async () => {
+    try {
+      console.log('i ran once')
+      const data = await getSubsEventsUsers()
+
+      for (const event of data) {
+        const currentTime = new Date().getTime()
+
+        const paymentDate = new Date(event.scheduleDate).getTime()
+        if (event.reminder) {
+          if (event.isEmailSent == false) {
+            const rminderTime = paymentDate - currentTime
+            if (rminderTime < 1) {
+              sendReminderEmail(event.email, event.scheduleDate, event.name)
+              const emailStatus = true
+              console.log('i have sent an loop')
+              const newEvent = await updateEmailStatus(event.id, emailStatus)
+              await updateEmailStatus(event.eventId, emailStatus)
+              console.log(newEvent)
+            }
+          }
+        }
+      }
+      console.log('i ran after loop')
+    } catch (error) {
+      console.error('An error occurred while running the cron job:', error)
+    }
+  })
+} else {
+  console.error('CRON_SCHEDULE is not defined')
+}
--- server/sendEmailDaily.ts
+++ server/sendEmailDaily.ts
@@ -1,40 +1,40 @@
-import cron from 'node-cron'
-import { getSubsEventsUsers } from './db/user'
-import { updateEmailStatus } from './db/events'
-import sendReminderEmail from './lib'
-require('dotenv').config()
+// import cron from 'node-cron'
+// import { getSubsEventsUsers } from './db/user'
+// import { updateEmailStatus } from './db/events'
+// import sendReminderEmail from './lib'
+// require('dotenv').config()
 
-const cronSchedule = process.env.CRON_SCHEDULE
+// const cronSchedule = process.env.CRON_SCHEDULE
 
-if (cronSchedule) {
-  cron.schedule(cronSchedule, async () => {
-    try {
-      console.log('i ran once')
-      const data = await getSubsEventsUsers()
+// if (cronSchedule) {
+//   cron.schedule(cronSchedule, async () => {
+//     try {
+//       console.log('i ran once')
+//       const data = await getSubsEventsUsers()
 
-      for (const event of data) {
-        const currentTime = new Date().getTime()
+//       for (const event of data) {
+//         const currentTime = new Date().getTime()
 
-        const paymentDate = new Date(event.scheduleDate).getTime()
-        if (event.reminder) {
-          if (event.isEmailSent == false) {
-            const rminderTime = paymentDate - currentTime
-            if (rminderTime < 1) {
-              sendReminderEmail(event.email, event.scheduleDate, event.name)
-              const emailStatus = true
-              console.log('i have sent an loop')
-              const newEvent = await updateEmailStatus(event.id, emailStatus)
-              await updateEmailStatus(event.eventId, emailStatus)
-              console.log(newEvent)
-            }
-          }
-        }
-      }
-      console.log('i ran after loop')
-    } catch (error) {
-      console.error('An error occurred while running the cron job:', error)
-    }
-  })
-} else {
-  console.error('CRON_SCHEDULE is not defined')
-}
+//         const paymentDate = new Date(event.scheduleDate).getTime()
+//         if (event.reminder) {
+//           if (event.isEmailSent == false) {
+//             const rminderTime = paymentDate - currentTime
+//             if (rminderTime < 1) {
+//               sendReminderEmail(event.email, event.scheduleDate, event.name)
+//               const emailStatus = true
+//               console.log('i have sent an loop')
+//               const newEvent = await updateEmailStatus(event.id, emailStatus)
+//               await updateEmailStatus(event.eventId, emailStatus)
+//               console.log(newEvent)
+//             }
+//           }
+//         }
+//       }
+//       console.log('i ran after loop')
+//     } catch (error) {
+//       console.error('An error occurred while running the cron job:', error)
+//     }
+//   })
+// } else {
+//   console.error('CRON_SCHEDULE is not defined')
+// }
