d79624fde02141adffbbbee312a8841cff83c390
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-05-06 15:30:23 +1200

    cron task testing

--- server/sendEmailDaily.ts
+++ server/sendEmailDaily.ts
@@ -3,7 +3,7 @@ import { getSubsEventsUsers } from './db/user'
 import { updateEmailStatus } from './db/events'
 import sendReminderEmail from './lib'
 
-cron.schedule('10 * * * *', async () => {
+cron.schedule('3 * * * *', async () => {
   try {
     
     console.log('i ran once')
